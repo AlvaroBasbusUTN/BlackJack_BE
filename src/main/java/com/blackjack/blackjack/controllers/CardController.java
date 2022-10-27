7285f37d1c5a097d17cd19a6c2f97b4148a716de
Alvarobasbus-112823 <69318172+Alvarobasbus@users.noreply.github.com>
2022-10-27 00:37:05 -0300

    Java

--- src/main/java/com/blackjack/blackjack/controllers/CardController.java
+++ src/main/java/com/blackjack/blackjack/controllers/CardController.java
@@ -19,18 +19,13 @@ public class CardController {
 
     private int[] numero = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13};
     private String[] suit=  {"c", "d", "t", "p"};
-    private Card card;
-
     private Card[] cards;
 
-    ArrayList<Card> list = new ArrayList<>();
 
 
     public CardController(){
-        card=new Card();
         cards=new Card[52];
-
-       barajar();
+        barajar();
     }
 
 
@@ -38,25 +33,13 @@ public class CardController {
 
     private void barajar(){
            int w=0;
-
             for (int j = 0; j < numero.length; j++){
                 for(int i=0; i< suit.length; i++) {
-
-                    //card.setSuit(suit[i]);
-                    //card.setNumber(numero[j]);
-                   // cards[1].setSuit(suit[i]);
                     cards[w] = new Card(numero[j], suit[i]);
-                    //cards[1].setNumber(numero[j]);
                     w+=1;
 
-                    list.add(card);
             }
-
-
         }
-
-
-
     }
 
     @GetMapping("/pedir/{num}")
@@ -68,20 +51,14 @@ public class CardController {
      @GetMapping("/pedir/")
     public String call(){
        int x= new Random().nextInt(52);
-       //list.get(x).isInGame();
-      //   if(list.get(numero).isInGame()==true){
-        // call();
-          //  }
-        // list.get(x).setInGame(true);
+        if(cards[x].isInGame()){
+        call();
+          }
+         cards[x].setInGame(true);
        String carta= x + " - " + cards[x].getNumber()  + cards[x].getSuit();
 
     return carta;
     }
-
-
-
-
-
 
 
 
